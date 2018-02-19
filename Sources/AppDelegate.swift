9b12044d8be76c439015ddeb0207f0d4830bbdcb
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2018-02-18 19:26:15 -0500

    Add files

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Pusher
+//
+//  Created by Jeff Kereakoglow on 2/18/18.
+//  Copyright © 2018 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pusher
+//
+//  Created by Jeff Kereakoglow on 2/18/18.
+//  Copyright © 2018 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
